22d2933327012d85b0473c2d74108457d4d26be0
annakrystalli <annakrystalli@googlemail.com>
2017-12-03 09:31:45 +0000

    export function

--- R/aoc_get_input.R
+++ R/aoc_get_input.R
@@ -6,11 +6,13 @@
 #' @param day numeric. Day for which input is required
 #' @param year numeric. Year for which input is required
 #' @param cookie_path character string. Path to the cookie string stored as a text file.
+#' @param encoding character string. Encoding of result. Defaults to "UTF-8".
 #' @return Returns user specific puzzle input as returned by the advent of code server.
 #' @import dplyr httr
 #' @details In chrome, get your cookie details by clicking on the triple dot menu
 #' (top right) > More tools > Developer tools > Application > Cookies. You need to copy the long
 #' string in the `value` column for row `session`. Save that as a text file.
+#' @export
 aoc_get_input <- function(day, year = 2017, cookie_path, encoding = "UTF-8"){
 
     if(!file.exists(cookie_path)){
@@ -24,4 +26,5 @@ aoc_get_input <- function(day, year = 2017, cookie_path, encoding = "UTF-8"){
         set_cookies("session" = cookie)) %>%
         content("text", encoding = encoding) %>% trimws()
 }
+
 
